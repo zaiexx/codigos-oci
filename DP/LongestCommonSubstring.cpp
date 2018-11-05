8e2293d1b90db98e529c3bb91de4cb049636120e
Esteban <esteban.naranjo.rayo@gmail.com>
2018-11-05 02:01:53 -0300

    LCSubsequence y LCSubstring

--- DP/LongestCommonSubstring.cpp
+++ DP/LongestCommonSubstring.cpp
@@ -19,15 +19,19 @@ int main(){
 		for(int b = 1; b <= t2; b++){
 			if(p1[a-1] == p2[b-1]){ //si los char son iguales 
 				mat[a][b] = mat[a-1][b-1] +1; //la pos actual es su diagonal +1
-				if(mat[a][b] > total) pos = a; //poner si se quiere saber cual es el substr
+				
+				if(mat[a][b] > total) pos = a; //poner si se quiere saber cual es el substr (no aplica a subsequence)
 				total = max(total, mat[a][b]); //el tamaño va a ser el max entre el guardado y la pos actual
+				//quitar la linea de arriba si se busca LCSubsequence
 			}else{
 				mat[a][b] = 0; //si no son iguales vale 0
+				//mat[a][b] = max(mat[a-1][b], mat[a][b-1]); //cambiar la linea de arriba por esta en caso de subsequence
 			}
 		}
 	}
 	
 	cout << total << "   : "<< p1.substr(pos-total,total); //imprimo el substr desde la pos de fin-total
+	//cout << mat[t1][t2]; //esto imprime el tamaño de la LCSubsequence
 	
 	return 0;
 }
